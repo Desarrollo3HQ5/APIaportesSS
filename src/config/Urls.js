739e8f670ee6d05a9cf794e81ba70a513d70af96
Juan-rojas75 <juancartagena75@hotmail.com>
2023-03-23 16:05:58 -0500

    comit 10

--- src/config/Urls.js
+++ src/config/Urls.js
@@ -41,7 +41,7 @@ async function AccesToken() {
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify(data), // body data type must match "Content-Type" header
     });
-    datos = JSON.stringify(response)
+    const datos = JSON.stringify(response)
     // return response.json(); // parses JSON response into native JavaScript objects
     return datos.json(); // parses JSON response into native JavaScript objects
   }
