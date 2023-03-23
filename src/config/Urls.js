b866bf416b078e078c795e70f67e6c49b5ad8c5a
Juan-rojas75 <juancartagena75@hotmail.com>
2023-03-23 16:06:57 -0500

    comit 11

--- src/config/Urls.js
+++ src/config/Urls.js
@@ -43,6 +43,6 @@ async function AccesToken() {
     });
     const datos = JSON.stringify(response)
     // return response.json(); // parses JSON response into native JavaScript objects
-    return datos.json(); // parses JSON response into native JavaScript objects
+    return datos; // parses JSON response into native JavaScript objects
   }
   export {AccesToken,postAPI}
