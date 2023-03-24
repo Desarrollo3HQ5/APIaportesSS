f8b7e7108c8156365797ec097a18cd8283352c7d
Juan-rojas75 <juancartagena75@hotmail.com>
2023-03-24 10:47:12 -0500

    comit final1

--- src/controller/controllAcesso.controller.js
+++ src/controller/controllAcesso.controller.js
@@ -18,7 +18,8 @@ ControlAccesoController.getEmployee = (req, res) => {
                 "Token":response.data
             // 'Content-Type': 'application/x-www-form-urlencoded',
             }
-            
+            console.log(headers)
+            console.log(data)
             postAPI(url,data,headers).then( (response) => {
                 console.log(response)
                 res.json(response)
