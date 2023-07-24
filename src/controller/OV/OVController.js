cd70284c88aba2627b60d2fb29f6de71ce27232e
Juan-rojas75 <juancartagena75@hotmail.com>
2023-07-24 15:43:13 -0500

    .

--- src/controller/OV/OVController.js
+++ src/controller/OV/OVController.js
@@ -10,7 +10,6 @@ OVController.add_user= async(req,res,next) => {
     // console.log(req.body)
     // const datos_ = JSON.stringify(req.body.data)
     // const datos_ = JSON.parse(req.body.data) 
-    console.log(req.body.data)
     const datos_  = req.body.data[0]
     // const datos_  = JSON.parse(JSON.stringify(req.body.data))
     var ids_ = []
